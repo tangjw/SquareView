7153008ba32dfb1f88f5d0a97a47f884191eabab
zsm-tang <tjwabc@gmail.com>
2016-06-20 19:03:40 +0800

    Initial commit

---
+++ app/src/main/java/com/zonsim/squareview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zonsim.squareview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		
+//		ScrollView scrollView = (ScrollView) findViewById(R.id.sv_items);
+		
+	}
+}
